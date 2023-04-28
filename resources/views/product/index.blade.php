0b58c4e12ce442d70aeaf857c5cdd503ff33218f
Manish Adz <manishadhikari096@gmail.com>
2023-04-29 00:02:04 +0545

    fix: table of product

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,55 +1,109 @@
 @extends('layouts.master')
 
 @section('content')
-    <section class="guest-houses">
+    <div class="customer__booking">
         <div class="container-lg">
-            <h1>
-                My Products
-                <a href="{{ route('products.create') }}" class="btn btn-primary float-end"><span class="bi bi-plus"></span> Add Product</a>
-            </h1>
-            <table class="table table-striped my-4">
-                <thead>
-                    <tr>
-                        <th scope="col">SN</th>
-                        <th scope="col">Images</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Compay</th>
-                        <th scope="col">Model</th>
-                        <th scope="col">min-price</th>
-                        <th scope="col">Max-price</th>
-                        <th scope="col">Endtime</th>
-                        <th scope="col">latest-bid</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($products as $product)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>
-                                <img src="{{ asset('uploads/product/' . $product->image) }}" height="60px;" widtd="60px;"
-                                    alt="Image of product">
-                            </td>
-                            <td>{{ $product->name }}</td>
-                            <td>{{ $product->company }}</td>
-                            <td>{{ $product->model }}</td>
-                            <td>{{ $product->min_price }}</td>
-                            <td>{{ $product->min_price }}</td>
-                            <td>{{ $product->end_time }}</td>
-                            <td>{{ @$product->users[$key]->pivot->bidding_amount }}</td>
-                            <td class="d-flex gap-2">
-                                <a href="{{ route('products.edit', $product->id) }}" class="btn btn-success"><span class="bi bi-pencil"></span></a>
-                                <form action="{{ route('products.destroy', $product->id) }}" method="post"
-                                    onclick="return confirm('Are you sure?')">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-danger"><span class="bi bi-trash"></span></button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="customer__booking-box">
+                <div class="row mb-4">
+                    <div class="col-sm-4">
+                        <h4 class="xl-title fw-bold text-dark mb-4">
+                            MY BOOKING LIST
+                        </h4>
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="row align-items-center justify-content-end">
+                            <div class="col-12 col-sm-6">
+                                <a href="{{ route('products.create') }}" class="btn btn-primary float-end"><span
+                                        class="bi bi-plus"></span> Add
+                                    Product</a>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div class="customer__booking-box-list">
+                    <ul class="m-0 p-0 all-hotel-listing list-style-type-none light-border-bottom-list list">
+                        @foreach ($products as $product)
+                            <li class="item">
+                                <div class="row align-items-center">
+                                    <div class="col-12 col-md-6 mb-4 mb-md-0">
+                                        <div class="d-flex align-items-center">
+                                            <div class="hotel-image-box">
+                                                <img src="{{ asset('uploads/product/' . $product->image) }}"
+                                                    alt="image of {{ $product->image }}" class="img-flui img-100-100">
+                                            </div>
+                                            <div class="text-start hotel-info ms-3">
+                                                <p class="location mb-2">
+                                                    {{$product->company}}
+                                                </p>
+                                                <h4 class="name mb-1">
+                                                    {{ $product->name }}
+                                                    ({{ $product->model }})
+                                                </h4>
+                                                <p class="location mb-2">
+                                                    {{$product->end_time}}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 col-md-2 booking-details">
+                                        <div class="text-start">
+                                            <div class="mb-2">
+                                                <p class="sm-title text-light-dark mb-1">
+                                                    Min Price
+                                                </p>
+                                                <h5 class="md-text text-dark mb-0">
+                                                    {{ $product->min_price }}
+                                                </h5>
+                                            </div>
+                                            <div class="mb-2">
+                                                <p class="sm-title text-light-dark mb-1">
+                                                    Max Price
+                                                </p>
+                                                <h5 class="md-text text-dark mb-0">
+                                                    {{ $product->max_price }}
+                                                </h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class=" col-2 col-md-2 booking-details">
+                                        <div class="text-start">
+                                            <span class="approved-status">
+                                               {{ @$product->users[$key]->pivot->bidding_amount}}
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div class="col-2 col-md-2">
+                                        <div class="list-hotel-dropdown">
+                                            <div class="d-flex justify-content-end">
+                                                <div class="dropdown-start d-inline-block">
+                                                    <img type="button"
+                                                        src="{{ asset('assets/images/menu/menu (1).png') }}"
+                                                        alt="menu-btn" data-bs-toggle="dropdown">
+                                                    <ul class="dropdown-menu shadow-lg">
+                                                        <li>
+                                                            <a href="{{ route('products.edit', $product->id) }}" class="dropdown-item"><span class="bi bi-pencil"></span> Edit</a>
+                                                        </li>
+                                                        <li>
+                                                            <form action="{{ route('products.destroy', $product->id) }}" method="post"
+                                                                onclick="return confirm('Are you sure?')">
+                                                                @csrf
+                                                                @method('delete')
+                                                                <button type="submit" class="dropdown-item booking-edit"><span class="bi bi-trash"></span> Delete</button>
+                                                            </form>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
         </div>
-    </section>
+    </div>
 @endsection
