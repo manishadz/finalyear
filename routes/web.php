c30cbee044a0ee1995c49165a8f553b9b5cf56fe
Manish Adz <manishadhikari096@gmail.com>
2023-04-29 12:35:12 +0545

    verify email fix

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ChangePassword;
-use App\Http\Controllers\HomeController;
-
 use App\Http\Controllers\BiddingController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
@@ -18,19 +15,17 @@ Route::get('/samusng-series', [FrontendController::class, 'samsung'])->name('sam
 
 Route::get('/product-detail/{id}', [FrontendController::class, 'detail'])->name('product');
 
+Auth::routes(["verify" => true]);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     // Route::resource('/category', CategoryController::class);
     Route::resource('/products', ProductController::class);
 
     Route::resource('/biddings', BiddingController::class);
+
     Route::get('/change-password', [FrontendController::class, 'password'])->name('updateform');
     Route::post('/change-password', [FrontendController::class, 'changePassword'])->name('update-password');
 });
-
-// backend routes
-Auth::routes(["verify" => true]);
-
 
 //notification
 Route::get('/send', [Notification::class, 'sendNotification']);
